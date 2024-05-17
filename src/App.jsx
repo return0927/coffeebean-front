edf43e52e0d89f94fa885d48ddc3f6efeb648f34
Eunhak Lee <lee@enak.kr>
2024-05-17 13:31:51 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>Hi there</h2>
+    </>
+  )
+}
+
+export default App
