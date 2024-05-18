7f601827c0701f49c4b4f96b048513aff734a853
Eunhak Lee <lee@enak.kr>
2024-05-19 04:00:51 +0900

    feat: 상단바 예제 디자인 추가

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -1,7 +1,43 @@
+import { useState } from 'react';
+import './TopMenu.css';
+
 const TopMenu = () => {
+    const [loginInfo, setLoginInfo] = useState(undefined);
+
+    const myPageBox = (loginInfo === undefined) ? (
+        <div className='menu-content'>
+            <label>Login</label>
+        </div>
+    ) : (<>
+        <div className='menu-content'>
+            <label>MyPage</label>
+        </div>
+        <div className='menu-content'>
+            <label>LogOut</label>
+        </div>
+    </>);
+``
     return (
-        <div>
-            Top Menu Bar
+        <div className='top-menu'>
+            <div className='menu-content logo'>
+                <img src='https://www.ajou.ac.kr/_res/ajou/kr/img/common/img-logo.png' />
+            </div>
+
+            <div className='menu-left'>
+                <div className='menu-content'>
+                    <span>Home</span>
+                </div>
+                <div className='menu-content'>
+                    <span>Products</span>
+                </div>
+                <div className='menu-content'>
+                    <span>Sellers</span>
+                </div>
+            </div>
+
+            <div className='menu-right'>
+                {myPageBox}
+            </div>
         </div>
     );
 };
