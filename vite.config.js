a3fd321a3d5def4a48a963911be1565f497c0b2b
Eunhak Lee <lee@enak.kr>
2024-05-22 03:49:02 +0900

    fix: react-router 가 이중으로 적용되는 오류

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import withReactRouter from 'vite-plugin-next-react-router';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    withReactRouter({
-      layout: '_layout',
-      pageDir: 'src/pages',
-      extensions: ['js', 'jsx'],
-    }),
-  ],
+  plugins: [react()],
 });
