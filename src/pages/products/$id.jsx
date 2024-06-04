8cc9d6a13a4d12694a8e88f4d8a81a66353f5e16
KDIGOM29 <118330238+KDIGOM29@users.noreply.github.com>
2024-06-04 13:19:12 +0900

    feat: 메인페이지에서 제품 상세페이지로 연결

--- src/pages/products/$id.jsx
+++ src/pages/products/$id.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import './id.css';
 
 // [id] 처럼 매칭하는 방법은 https://nextjs.org/docs/pages/building-your-application/routing/dynamic-routes 를 참고해주세요
 const ProductDetail = () => {
   const { id } = useParams();
   const [data, setData] = useState(undefined);
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     async function fetcher() {
@@ -16,10 +18,32 @@ const ProductDetail = () => {
     fetcher();
   }, [id]);
 
+  const handleQuantityChange = (amount) => {
+    setQuantity((prev) => Math.max(1, prev + amount));
+  };
+
   if (data === undefined) return <>Loading...</>;
   return (
-    <div>
-      <>{JSON.stringify(data)}</>
+    <div className="product-detail-container">
+      <div className="product-image">이미지</div>
+      <div className="product-info">
+        <h1>{data.name}</h1>
+        <ul>
+          <li>원산지: {data.origins}</li>
+          <li>용량: {data.quantity}g</li>
+          <li>판매자: {data.brandName}</li>
+          <li>가공방식: {data.processing}</li>
+          <li>분쇄도: {data.grinding}</li>
+          <li>
+            개수: 
+            <button onClick={() => handleQuantityChange(-1)}>-</button>
+            <input type="text" value={quantity} readOnly />
+            <button onClick={() => handleQuantityChange(1)}>+</button>
+          </li>
+          <li>판매가: {data.price*quantity}원</li>
+        </ul>
+        <button className="buy-button">BUY</button>
+      </div>
     </div>
   );
 };
