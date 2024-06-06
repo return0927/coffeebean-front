5eedad7452a2cc26fbcc038c7e4a5ee98abb1a04
bumjuni <darlian0889@gmail.com>
2024-06-06 17:15:53 +0900

    feat: 상품 추가하기

--- src/pages/partners/myProducts/addproducts.jsx
+++ src/pages/partners/myProducts/addproducts.jsx
@@ -1,6 +1,73 @@
 import '../index.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import loginState from '../../../state';
 
-const addproducts = () => {
+const Addproducts = () => {
+  const navigate = useNavigate();
+  const setLoginData = useRecoilState(loginState)[1];
+
+  const [discounts, setDiscounts] = useState(0);
+  const [grinding, setGrinding] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [name, setName] = useState('');
+  const [origins, setOrigins] = useState('');
+  const [price, setPrice] = useState(0);
+  const [processing, setProcessing] = useState('');
+  const [quantity, setQuantity] = useState(0);
+
+  const validateInputs = () => {
+    if (
+      [
+        discounts,
+        grinding,
+        imageUrl,
+        name,
+        origins,
+        price,
+        processing,
+        quantity,
+      ].some((value) => !value)
+    )
+      return '빈 필드가 있습니다.';
+  };
+
+  const handleAdd = async () => {
+    const validationMessage = validateInputs();
+    if (validationMessage) {
+      alert(validationMessage);
+      return;
+    }
+
+    const resp = await fetch(`/products`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: JSON.stringify({
+        discounts,
+        grinding,
+        imageUrl,
+        name,
+        origins,
+        price,
+        processing,
+        quantity,
+      }),
+    });
+    const data = await resp.json();
+
+    // if error
+    if (data.error) {
+      const { message } = data;
+      alert(`상품 추가 중 오류가 발생했습니다. ${message}`);
+      navigate(0);
+    } else {
+      navigate('/index');
+    }
+  };
+
   return (
     <div className='SignIncontainer'>
       <aside className='SigninImage'>
@@ -17,7 +84,9 @@ const addproducts = () => {
           <input
             type='text'
             name='name'
+            value={name}
             placeholder={'상품 이름을 입력하세요'}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div>
@@ -25,7 +94,9 @@ const addproducts = () => {
           <input
             type='text'
             name='origin'
+            value={origins}
             placeholder={'원산지를 입력하세요'}
+            onChange={(e) => setOrigins(e.target.value)}
           />
         </div>
         <div>
@@ -33,7 +104,9 @@ const addproducts = () => {
           <input
             type='text'
             name='capacity'
+            value={quantity}
             placeholder={'용량을 입력하세요'}
+            onChange={(e) => setQuantity(e.target.value)}
           />
         </div>
         <div>
@@ -45,7 +118,9 @@ const addproducts = () => {
           <input
             type='text'
             name='processing'
+            value={processing}
             placeholder={'가공 방식을 입력하세요'}
+            onChange={(e) => setProcessing(e.target.value)}
           />
         </div>
         <div>
@@ -53,7 +128,9 @@ const addproducts = () => {
           <input
             type='text'
             name='grinding'
+            value={grinding}
             placeholder={'분쇄도를 입력하세요'}
+            onChange={(e) => setGrinding(e.target.value)}
           />
         </div>
         <div>
@@ -61,15 +138,23 @@ const addproducts = () => {
           <input
             type='text'
             name='discount'
+            value={discounts}
             placeholder={'할인율을 입력하세요'}
+            onChange={(e) => setDiscounts(e.target.value)}
           />
         </div>
         <div>
           판매가
-          <input type='text' name='price' placeholder={'판매가를 입력하세요'} />
+          <input
+            type='text'
+            name='price'
+            value={price}
+            placeholder={'판매가를 입력하세요'}
+            onChange={(e) => setPrice(e.target.value)}
+          />
         </div>
 
-        <button type='button' className='SignInButton' onClick={''}>
+        <button type='button' className='SignInButton' onClick={handleAdd}>
           등록하기
         </button>
       </div>
@@ -77,4 +162,4 @@ const addproducts = () => {
   );
 };
 
-export default addproducts;
+export default Addproducts;
